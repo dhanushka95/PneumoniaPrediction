4ec8d1df8c070184017d889f50b8118929d798b0
dhanushka95 <danushkadayawansha44@gmail.com>
2020-12-06 22:01:56 +0530

    add cors

--- node server/app.js
+++ node server/app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static('uploads'));
 
 // Admin API
 app.use('/api', require('./router'));
-require('./controller').getWeightForModel();
+//require('./controller').getWeightForModel();
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 
--- node server/router.js
+++ node server/router.js
@@ -25,33 +25,25 @@ router.post(
         }
         try{
             const Path = require('path');
-            const form = new FormData();
-            form.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
+            const form_1 = new FormData();
+            const form_2 = new FormData();
+            form_1.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
+                filename: file.filename
+            });
+            form_2.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
                 filename: file.filename
             });
             req.headers.PredictData = [];
             // get prediction from api 1
             const API_01 = await axios.create({
-                headers: form.getHeaders()
-            }).post('http://127.0.0.1:5001/predict', form);
+                headers: form_1.getHeaders()
+            }).post('http://127.0.0.1:5001/predict', form_1);
             const API_01_VALUE = API_01.data;
             const API_02 = await axios.create({
-                headers: form.getHeaders()
-            }).post('http://127.0.0.1:5002/predict', form);
+                headers: form_2.getHeaders()
+            }).post('http://127.0.0.1:5002/predict', form_2);
             const API_02_VALUE = API_02.data;
-            let positiveAccuracy =0;
-            if(API_01_VALUE===1 && API_02_VALUE==1){
-                positiveAccuracy=93.91025;
-            }
-            if(API_01_VALUE===1 && API_02_VALUE==0){
-                positiveAccuracy=65;
-            }
-            if(API_01_VALUE===0 && API_02_VALUE==1){
-                positiveAccuracy=0;
-            }
-            if(API_01_VALUE===0 && API_02_VALUE==0){
-                positiveAccuracy=8.108108;
-            }
+            let positiveAccuracy = (API_01_VALUE+API_02_VALUE)/2;
             res.status(200).json({
                 data: positiveAccuracy,
                 message: 'Prediction'

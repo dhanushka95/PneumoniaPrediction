a6d0c99e9a6bc0aa3d9963b4667af3c589941e3f
dhanushka95 <danushkadayawansha44@gmail.com>
2020-12-18 09:24:48 +0530

    create AXIOs call , to final api

--- node server/router.js
+++ node server/router.js
@@ -25,18 +25,36 @@ router.post(
         }
         try{
             const Path = require('path');
+            const form_1 = new FormData();
             const form_2 = new FormData();
+            /*
+            form_1.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
+                filename: file.filename
+            });
             form_2.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
                 filename: file.filename
             });
             req.headers.PredictData = [];
+            const API_01 = await axios.create({
+                headers: form_1.getHeaders()
+            }).post('http://127.0.0.1:5001/predict', form_1);
+            const API_01_VALUE = API_01.data;
             const API_02 = await axios.create({
                 headers: form_2.getHeaders()
             }).post('http://127.0.0.1:5002/predict', form_2);
             const API_02_VALUE = API_02.data;
-            let positiveAccuracy = ((API_02_VALUE))*100;
+            let positiveAccuracy = ((API_01_VALUE+API_02_VALUE)/2)*100;
+            */
+            const form_final = new FormData();
+            form_final.append('image', fs.createReadStream(Path.join('./uploads/'+file.filename)), {
+                filename: file.filename
+            });
+            const API_FINAL = await axios.create({
+                headers: form_final.getHeaders()
+            }).post('http://127.0.0.1:5003/predict', form_final);
+            const API_FINAL_VALUE = API_FINAL.data;
             res.status(200).json({
-                data: positiveAccuracy,
+                data: API_FINAL_VALUE,
                 message: 'Prediction'
             });
         }catch (e) {

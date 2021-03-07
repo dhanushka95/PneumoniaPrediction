5485f89ced4e9d8963c6eb25e8d48fc06cdce0c2
dhanushka95 <danushkadayawansha44@gmail.com>
2021-03-07 09:07:56 +0530

    add ddos npm library to prevent ddos attack

--- node server/app.js
+++ node server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require('cors')
+var cors = require('cors');
+const Ddos = require('ddos');
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(new Ddos().express);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '50mb' }));
